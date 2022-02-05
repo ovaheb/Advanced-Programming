101b4022daf90a426e38e6dba6be59709de84bfd
ovaheb <ovaheb@gmail.com>
2022-02-05 18:46:27 +0330

    Minor rearrangements

--- Online-Market/Phase 1/cart.cpp
+++
@@ -1,8 +0,0 @@
-#include "cart.hpp"
-
-void Cart::addItem(Offer* chosenOffer, int quantity, float discount)
-{
-    offers.push_back(chosenOffer);
-    quantities.push_back(quantity);
-    percentage.push_back(discount);
-}
--- Online-Market/Phase 1/user.cpp
+++
@@ -1,8 +0,0 @@
-#include "user.hpp"
-
-size_t wordHasher(string word)
-{
-    hash<string> hasher;
-    size_t result = hasher(word);
-    return result;
-}
